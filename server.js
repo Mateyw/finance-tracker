17dd90cf631a133980da344a1cbfc4fba6d0ba97
Mateyw <mateusz.mazik@adidas.com>
2025-02-14 17:31:04 +0100

    ADD comments

--- server.js
+++ server.js
@@ -1,20 +1,24 @@
-// Erstellen einer neuen express Anwendung
-const express = require('express');
+// Create a new express application
+const express = require("express");
 const app = express();
+// Import mysql library
+const mysql = require("mysql2");
+// Import passwort-hashing library bcrypt
+const bcrypt = require("bcryptjs");
+// Import session library
+const session = require("express-session");
 
 app.set("view engine", "ejs");
-// Middleware zum Parsen von URL-kodierten Daten
+// Middleware to parse URL-encoded data
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-
-// Middleware zum Parsen von JSON-Daten
+// Middleware to parse JSON data
 app.use(express.json());
-
 
 const PORT = process.env.PORT || 8000;
 
-// Server auf angegebenen Port starten
+// Start server on specified port
 app.listen(PORT, () => {
     console.log(`Server listening on Port ${PORT}`);
 });
