1ae340016c67aa66b3b541dc4b67743aab2b24cd
Mateyw <mateusz.mazik@adidas.com>
2025-02-23 17:11:49 +0100

    ADD charts container and styles

---
+++ public/js/charts/pie-chart.js
@@ -0,0 +1,20 @@
+const pieChart = document.getElementById('pieChart');
+
+new Chart(pieChart, {
+    type: 'pie',
+    data: {
+      labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+      datasets: [{
+        label: '# of Votes',
+        data: [12, 19, 3, 5, 2, 3],
+        borderWidth: 1
+      }]
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      }
+    }
+  });
